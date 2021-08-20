1e23e4c71d7edb8a3c699922861e2285acf7adf6
Kadir Olmez <kadir.olmez.y2349@student.hogent.be>
2021-08-20 08:48:42 +0200

    backend

--- WindowsApplicatie_NetteVersie/Services/AuthService.cs
+++ WindowsApplicatie_NetteVersie/Services/AuthService.cs
@@ -1,6 +1,7 @@
 ﻿
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -70,7 +71,68 @@ namespace WindowsApplicatie_NetteVersie
             return (u, c);
 
         }
+
+        public static async Task<(User, CustomError)> Register(string email, string firstn, string lastn, string phone, string password, string passwordC, DateTimeOffset date)
+        {
+            HttpClient client = new HttpClient();
+            User u = new User();
+            CustomError c = new CustomError();
+
+            try
+            {
+                var registerInput = new
+                {
+                    email = email,
+                    password = password,
+                    firstName = firstn,
+                    lastName=lastn,
+                    phone = phone,
+                    passwordConfirmation = passwordC,
+                    dateOfBirth = date
+                };
+
+                var stringPayload = JsonConvert.SerializeObject(registerInput);
+                var httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
+                var result = await client.PostAsync("https://na2backend.azurewebsites.net/api/user/register", httpContent);
+
+
+
+                if (result.Content != null)
+                {
+                    var responseContent = await result.Content.ReadAsStringAsync();
+                    try
+                    {
+                        u = (User)JObject.Parse(responseContent);
+                        AppUser = u;
+                    }
+                    catch
+                    {
+                        if (responseContent.ToLower().Contains("password"))
+                        {
+                            c.Scope = "password";
+                            c.Message = responseContent;
+                        }
+                        else if (responseContent.ToLower().Contains("email"))
+                        {
+                            c.Scope = "email";
+                            c.Message = responseContent;
+                        }
+                        else
+                        {
+                            c.Scope = "app";
+                            c.Message = responseContent;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                c.Scope = "app";
+                c.Message = "Something went wrong with the connection to the server. Please contact your admin!";
+            }
+
+            return (u, c);
+
+        }
     }
-
-
 }
